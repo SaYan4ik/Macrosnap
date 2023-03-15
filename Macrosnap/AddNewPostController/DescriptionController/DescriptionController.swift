2249f290dcf7475b20df71fc9f930ddee877a42e
SaYan4ik <66200306+SaYan4ik@users.noreply.github.com>
2023-03-15 15:49:25 +0300

    remove no need func

--- Macrosnap/AddNewPostController/DescriptionController/DescriptionController.swift
+++ Macrosnap/AddNewPostController/DescriptionController/DescriptionController.swift
@@ -169,7 +169,6 @@ extension DescriptionController {
         let widthRatio  = targetSize.width  / size.width
         let heightRatio = targetSize.height / size.height
         
-        // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
             newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
@@ -177,10 +176,8 @@ extension DescriptionController {
             newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
         }
         
-        // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(origin: .zero, size: newSize)
         
-        // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
